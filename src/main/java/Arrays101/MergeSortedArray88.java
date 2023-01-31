6c146513c47caa671bc0ad8e2ce930937079b98b
haein.sung <blue6zombie6@gmail.com>
2023-01-31 16:11:35 +0900

    2023/1/31 Merge Sorted Array 88 (Easy) (Own) i thought the zeros could be mixed but apparently not...

--- src/main/java/Arrays101/MergeSortedArray88.java
+++ src/main/java/Arrays101/MergeSortedArray88.java
@@ -5,14 +5,9 @@ import java.util.Arrays;
 public class MergeSortedArray88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int index = 0;
-        for (int i = 0; i < m + n; i++) {
-            if (nums1[i] == 0) {
-                nums1[i] = nums2[index];
-                index += 1;
-            }
-            if (index == n) { // if i don't put this, runtime error(index) coming
-                break;
-            }
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[index];
+            index += 1;
         }
         Arrays.sort(nums1);
     }
